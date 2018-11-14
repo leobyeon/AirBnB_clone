4b2be229e2a4538837f6993412e0051ed2d5cc99
leobyeon <386@holbertonschool.com>
2018-11-14 21:31:06 +0000

    Changed initial setting of updated_at

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
         else:
             for k, v in kwargs.items():
