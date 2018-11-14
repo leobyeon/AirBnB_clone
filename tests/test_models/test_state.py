b0e7552f216424758116441776621ba7ef9164fd
anovacap <anovacap@yahoo.com>
2018-11-14 20:52:54 +0000

    initial commit

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -2,11 +2,23 @@
 """Unit test for class State"""
 import unittest
 from models.base_model import BaseModel
+from models.state import State
+from unittest.mock import create_autospec
 import sys, os, pep8
-
+from console import HBNBCommand
+import tests
 
 class TestState(unittest.TestCase):
     """Tests for class BaseModel"""
+    def setUp(self):
+        """Set up for the tests"""
+        self.my_model1 = State()
+        self.my_model1.name = "Me"
+        self.my_model2 = State()
+        self.my_model2.name = "goodbye"
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
+
     def test_state(self):
         """test for review"""
-        pass
+        self.assertEqual(self.my_model1.name, "Me")
