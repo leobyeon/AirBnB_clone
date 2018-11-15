c82247fee28799f336acaca5f70ac66e3a07223c
anovacap <anovacap@yahoo.com>
2018-11-15 02:37:07 +0000

    test new, save, pep8

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -8,6 +8,24 @@ import os
 import pep8
 from console import HBNBCommand
 from models import storage
+import json
+import models
+
+
+def setUpModule():
+    ob = storage.all()
+    ob.clear()
+    storage.save()
+    if os.path.isfile('file.json'):
+        os.remove('file.json')
+
+
+def tearDownModule():
+    ob = storage.all()
+    ob.clear()
+    storage.save()
+    if os.path.isfile('file.json'):
+        os.remove('file.json')
 
 
 class TestBaseModel(unittest.TestCase):
@@ -45,6 +63,23 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(self.my_model1.name, "hello")
         self.assertEqual(type(self.my_model1.number), int)
         self.assertEqual(self.my_model1.number, 9)
+
+    def test_save(self):
+        self.my_model1.save()
+        with open('file.json') as f:
+            jd = f.read()
+            pyob = json.loads(jd)
+            k = pyob.keys()
+        for key in models.storage.all().keys():
+            self.assertTrue(key in k)
+
+    def test_pep8(self):
+        """test if module is pep8 compliant"""
+        pep = pep8.StyleGuide(quiet=False)
+        sty = pep.check_files(["models/engine/file_storage.py",
+                               "tests/test_models/" +
+                               "test_engine/test_file_storage.py"])
+        self.assertEqual(sty.total_errors, 0, "PEP8 Errors")
 
 if __name__ == '__main__':
     unittest.main()
